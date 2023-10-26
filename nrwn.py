469ad5c7033bc90efca37dee91c2dd8411ad7146
nirwaneu <nirwaneu@users.noreply.github.com>
2023-10-26 15:46:55 +0700

    Create nrwn.py

---
+++ nrwn.py
@@ -0,0 +1,100 @@
+AUTOMATIC1111
+stable-diffusion-webui
+sd-webui
+sd-webui-controlnet
+stable-diffusion-webui-images-browser
+sd-webui
+sd-webui-cutoff
+sd-webui-regional-prompter
+sd-webui-tunnels
+sd-webui-roop
+pycloudflared
+insightface==0.7.3
+onnx==1.14.0
+onnxruntime==1.15.0
+opencv-python==4.7.0.72
+tqdm
+https://github.com/AUTOMATIC1111/stable-diffusion-webui
+https://github.com/Iyashinouta/sd-model-downloader
+https://github.com/Coyote-A/ultimate-upscale-for-automatic1111
+https://github.com/Mikubill/sd-webui-controlnet
+https://github.com/A-I-Channel/public-link /notebooks/stable-diffusion-webui/extensions/tunnels
+https://github.com/A-I-Channel/explorer-image
+/notebooks/stable-diffusion-webui/config.json
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_canny.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_canny_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_openpose.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_openpose_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11f1e_sd15_tile.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11f1e_sd15_tile_fp16.safetensors
+/notebooks/stable-diffusion-webui/embeddings/embeddings.zip https://github.com/A-I-Channel/Data-file/raw/main/embeddings.zip
+/notebooks/stable-diffusion-webui/models/ESRGAN/upscalers.zip https://huggingface.co/datasets/AI-C/Data-File/resolve/main/upscalers.zip
+/notebooks/stable-diffusion-webui/embeddings
+/notebooks/stable-diffusion-webui/embeddings/embeddings.zip
+embeddings.zip
+/notebooks/stable-diffusion-webui/models/ESRGAN
+/notebooks/stable-diffusion-webui/models/ESRGAN/upscalers.zip
+upscalers.zip
+/content
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.safetensors
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.png
+https://huggingface.co/Kizi-Art/Lora-Clone/resolve/main/add_detail.jpeg
+https://github.com/IDEA-Research/DWPose
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_canny.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_canny_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_inpaint.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_inpaint_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_openpose.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_openpose_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15s2_lineart_anime.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15s2_lineart_anime_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11f1e_sd15_tile.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11f1e_sd15_tile_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11f1p_sd15_depth.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_depth_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_seg.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_seg_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_mediapipe_face_sd15_v2.safetensors https://huggingface.co/lokCX/control_mediapipe_face/resolve/main/control_mediapipe_face_sd15_v2.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11e_sd15_ip2p.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11e_sd15_ip2p_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11e_sd15_shuffle.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11e_sd15_shuffle_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_lineart.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_lineart_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_mlsd.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_mlsd_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_normalbae.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_normalbae_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_scribble.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_scribble_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/control_v11p_sd15_softedge.safetensors https://huggingface.co/nolanaatama/models/resolve/main/control_v11p_sd15_softedge_fp16.safetensors
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_canny_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_canny_sd14v1.pth
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_color_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_color_sd14v1.pth
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_depth_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_depth_sd14v1.pth
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_keypose_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_keypose_sd14v1.pth
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_openpose_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_openpose_sd14v1.pth
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_seg_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_seg_sd14v1.pth
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_sketch_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_sketch_sd14v1.pth
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/t2iadapter_style_sd14v1.pth https://huggingface.co/nolanaatama/models/resolve/main/t2iadapter_style_sd14v1.pth
+https://github.com/A-I-Channel/sdw-wd14-tagger
+https://github.com/fkunn1326/openpose-editor
+https://github.com/A-I-Channel/sd-openpose-editor-clone
+-b v2.5 https://github.com/camenduru/ui /notebooks/stable-diffusion-webui
+https://github.com/A-I-Channel/ui-clone /notebooks/stable-diffusion-webui
+https://github.com/A-I-Channel/AI_Channel_Theme
+https://github.com/deforum-art/sd-webui-deforum
+https://github.com/Bing-su/adetailer
+https://github.com/hnmr293/sd-webui-cutoff
+https://github.com/Gourieff/sd-webui-reactor
+https://huggingface.co/datasets/AI-C/Data-File/resolve/main/inswapper_128.onnx
+https://github.com/bit9labs/sd-ratio-lock
+https://github.com/brick2face/seamless-tile-inpainting
+/notebooks/stable-diffusion-webui/models/Stable-diffusion/cyberrealistic_v33.safetensors https://civitai.com/api/download/models/138176
+/notebooks/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/ioclab_sd15_recolor.safetensors https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/ioclab_sd15_recolor.safetensors
+install xformers==0.0.20 triton==2.0.0 -U
+uninstall -y torch
+uninstall -y torchvision
+uninstall -y torchaudio
+install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+apt -y update -qq
+apt -y install -qq aria2 libcairo2-dev pkg-config python3-dev
+apt-get update
+apt-get install -y google-perftools
+apt -y install wget git python3 python3-venv
+/notebooks/
+pip install mediapipe
+pip install ultralytics
+pip install insightface==0.7.3
+--share --disable-safe-unpickle --theme=dark --no-half-vae --enable-insecure-extension --gradio-queue
+https://github.com/etherealxx/batchlinks-webui
+https://github.com/A-I-Channel/Downloader
+https://github.com/DominikDoom/a1111-sd-webui-tagcomplete
+-y -c conda-forge opencv
+https://github.com/pharmapsychotic/clip-interrogator-ext
+https://github.com/A-I-Channel/tagger-v2
+Stable-diffusion
+https://github.com/huchenlei/sd-webui-openpose-editor
